d8847e56bdd164b2e3cbe8d57f1018e10aaf407d
TaeYoung1030 <taeyoung.oc84@gmail.com>
2025-09-25 11:24:11 +0900

    java test2

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello 
+{
+    private String talk;
+    public Hello()
+    {
+        talk = "Hello";
+    }
+
+    public Hello(String talk)
+    {
+        this.talk = talk;
+    }
+}
